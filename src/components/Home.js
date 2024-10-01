01ad4205544197f07dea339d03ee5700022f526a
Rimsha Ubaid <rimshaubaid@Rimshas-MacBook-Air.local>
2024-10-01 16:24:11 +0500

    pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import HomeImg from "../assets/images/home.jpeg";
 const Home = () => {
   return (
     <Box sx={{ paddingTop: 7 }}>
-      <img src={HomeImg} />
+      <img src={HomeImg} style={{width:"100%",height:"95vh"}} />
     </Box>
   );
 };
